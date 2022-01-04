78c1ac24ca41868f3f012d0a8e1c253ae8cca7bf
lsh <jetaime12121@gmail.com>
2022-01-04 20:07:16 +0900

    프로젝트 세팅

---
+++ front/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ClosetView from '@/views/ClosetView.vue'
+import DetailView from '@/views/DetailView.vue'
+import MainView from '@/views/MainView.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'MainView',
+    component: MainView
+  },
+  {
+    path: '/closet',
+    name: 'ClosetView',
+    component: ClosetView
+  },
+  {
+    path: '/detail',
+    name: 'DetailView',
+    component: DetailView
+  },
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ front/src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <router-link to='/detail'>상세</router-link>
+      <router-link to='/closet'>옷장</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    a {margin:20px; font-size: 100px; text-decoration: none; color:brown;}
+</style>
