61ba5835c7ac9a92ce740680d06835f152a4ea91
lsh <jetaime12121@gmail.com>
2022-01-08 15:06:00 +0900

    my closet
    category 별 배열 생성
    category 클릭 시 다른 배열 등장

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -6,20 +6,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    clothes:[
-      
-    ]
+    clothes_outer:[],
+    clothes_top:[],
+    clothes_pants:[],
+    clothes_socks:[],
+    clothes_shoes:[],
   },
   mutations: {
     SET_CLOTHES(state, clothes){
-      state.clothes = clothes;
+      //filter 함수로 조건에 맞는 배열 생성 
+      //1.outer
+      state.clothes_outer = clothes.filter(item => item.cate === "outer");
+      //2.top
+      state.clothes_top = clothes.filter(item => item.cate === "top");
+      //3.pants
+      state.clothes_pants = clothes.filter(item => item.cate === "pants");
+      //4.socks
+      state.clothes_socks = clothes.filter(item => item.cate === "socks");
+      //5.shoes
+      state.clothes_shoes = clothes.filter(item => item.cate === "shoes");
     }
   },
   actions: {
-    
     //옷장 가져오기
     async GET_CLOTHES(context){
       const response = await getClothes();
+      console.log(response.data);
       context.commit('SET_CLOTHES', response.data)
     }
   },
